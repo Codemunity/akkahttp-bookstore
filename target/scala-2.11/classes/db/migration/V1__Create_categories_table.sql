a16d89f2e5440a2d97fc2b1a7c1000c3de1e4d59
Miguel Lopez V <mlopezva@akamai.com>
2017-01-19 01:56:11 -0600

    Apply gitignore file

--- target/scala-2.11/classes/db/migration/V1__Create_categories_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "categories" (
-  "id"          BIGSERIAL PRIMARY KEY,
-  "title"       VARCHAR NOT NULL
-);
-ALTER TABLE categories ADD CONSTRAINT categories_unique_title UNIQUE (title);
